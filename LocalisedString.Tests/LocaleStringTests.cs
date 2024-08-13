bdb45985c73279e5e7916e1d3017fd8a89835f28
Simon Jefferies <simon@olivegame.studio>
2024-08-13 21:26:33 +0100

    Add unit tests.

--- LocalisedString.Tests/LocaleStringTests.cs
+++ LocalisedString.Tests/LocaleStringTests.cs
@@ -1,10 +1,40 @@
 using System.Globalization;
 using Musts;
 
-namespace LocalisedString;
+namespace LocalisedString.Tests;
 
 public class LocaleStringTests
 {
+    [Test]
+    public void EnglishGB_Returns_EnglishText_FromRealSetup()
+    {
+        LocaleString s = new();
+        s.AddTranslation(new CultureInfo("en"), "EnglishText");
+        s.AddTranslation(new CultureInfo("fr"), "FrenchText");
+        s.AddTranslation(new CultureInfo("en-GB"), "EnglishGBText");
+        s.AddTranslation(new CultureInfo("fr-fr"), "FrenchFRText");
+        string result = s.GetLocalisedString(new CultureInfo("en-GB"));
+
+        result.MustBeSame("EnglishGBText");
+    }
+
+    [Test]
+    public void AddingTranslationTwice_ThrowsException()
+    {
+        LocaleString s = new();
+        s.AddTranslation(new CultureInfo("en"), "EnglishText");
+        Assert.Throws<LocaleStringException>(() => s.AddTranslation(new CultureInfo("en"), "EnglishText"));
+    }
+
+    [Test]
+    public void English_Returns_EmptyString()
+    {
+        LocaleString s = new();
+        string result = s.GetLocalisedString(new CultureInfo("en"));
+
+        result.MustBeNullOrEmpty();
+    }
+
     [Test]
     public void EnglishGB_Returns_EnglishText()
     {
@@ -16,6 +46,16 @@ public class LocaleStringTests
     }
 
     [Test]
+    public void EnglishGB_Returns_EnglishGBText()
+    {
+        LocaleString s = new();
+        s.AddTranslation(new CultureInfo("en-GB"), "EnglishGBText");
+        string result = s.GetLocalisedString(new CultureInfo("en-GB"));
+
+        result.MustBeSame("EnglishGBText");
+    }
+
+    [Test]
     public void English_Returns_EnglishText()
     {
         LocaleString s = new();
@@ -23,5 +63,15 @@ public class LocaleStringTests
         string result = s.GetLocalisedString(new CultureInfo("en"));
 
         result.MustBeSame("EnglishText");
+    }
+
+    [Test]
+    public void English_Returns_InvariantText()
+    {
+        LocaleString s = new();
+        s.AddTranslation(CultureInfo.InvariantCulture, "InvariantText");
+        string result = s.GetLocalisedString(new CultureInfo("en"));
+
+        result.MustBeSame("InvariantText");
     }
 }
